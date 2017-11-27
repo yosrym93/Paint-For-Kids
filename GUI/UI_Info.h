a61339568b5d4dbcb7497256ae95d9f6d653a2c5
Shady <shadyfahmy67@gmail.com>
2017-11-27 14:48:05 +0200

    updating with phase 1 files

--- GUI/UI_Info.h
+++ GUI/UI_Info.h
@@ -10,35 +10,62 @@
 enum GUI_MODE	//Graphical user interface mode
 {
 	MODE_DRAW,	//Drawing mode (startup mode)
-	MODE_PLAY	//Playing mode
+	MODE_PLAY,	//Playing mode
+	MODE_COLOR	//Color Selector mode
 };
 
-enum DrawMenuItem //The items of the Draw menu (you should add more items)
+enum DrawMenuItem //The items of the Draw menu 
 {
 	//Note: Items are ordered here as they appear in menu
-	//If you want to change the menu items order, change the order here
-	ITM_RECT,		//Recangle item in menu
-	ITM_CIRC,		//Circle item in menu
-	
-	//TODO: Add more items names here
+	ITM_LINE,				//Line item in menu
+	ITM_RECT,				//Recangle item in menu
+	ITM_TRIG,				//Triangle item in menu
+	ITM_CIRC,				//Circle item in menu
+	ITM_CHNG_DRAW_CLR,		//Change draw color item in menu
+	ITM_CHNG_FILL_CLR,		//Change fill color item in menu
+	ITM_SELECT,				//Select item in menu
+	ITM_DELETE,				//Delete item in menu
+	ITM_COPY,				//Copy item in menu
+	ITM_CUT,				//Cut item in menu
+	ITM_PASTE,				//Paste item in menu
+	ITM_ROTATE,				//Rotate item in menu
+	ITM_BTF,				//Bring to front item in menu
+	ITM_STB,				//Send to back item in menu
+	ITM_SAVE,				//Save item in menu
+	ITM_LOAD,				//Load item in menu
+	ITM_TO_PLAY,			//To Play mode item in menu
+	ITM_EXIT,				//Exit item
 
-	ITM_EXIT,		//Exit item
-	
-	DRAW_ITM_COUNT		//no. of menu items ==> This should be the last line in this enum
+	DRAW_ITM_COUNT			//no. of menu items ==> This should be the last line in this enum
 	
 };
 
 enum PlayMenuItem //The items of the Play menu (you should add more items)
 {
-	//Note: Items are ordered here as they appear in menu
-	//If you want to change the menu items order, change the order here
+	ITM_P_H_TYPE,		//Play and hide by type item in menu
+	ITM_P_H_COLOR,		//Play and hide by color item in menu
+	ITM_P_H_BOTH,		//Play and hide by both color and type item in menu
+	ITM_TO_DRAW,		//To draw mode item in menu
+	ITM_EXIT2,			//Exit item in menu
 	
-	//TODO: Add more items names here
 
 	PLAY_ITM_COUNT		//no. of menu items ==> This should be the last line in this enum
 	
 };
 
+
+enum ColorMenuItem //The items of the Color menu (you should add more items)
+{
+	ITM_BLACK,		//Black color item in menu
+	ITM_WHITE,		//White color item in menu	
+	ITM_RED,		//Red color item in menu
+	ITM_GREEN,		//Green color item in menu
+	ITM_BLUE,		//Blue color item in menu
+	ITM_EXIT3,		//Exit item in menu
+
+	COLOR_ITM_COUNT		//no. of menu items ==> This should be the last line in this enum
+
+};
 
 
 
@@ -49,7 +76,7 @@ struct UI_Info	//User Interface Info.
 {
 	GUI_MODE InterfaceMode;
 	
-	int	width, height,	//Window width and height
+	int	width, height,	    //Window width and height
 		wx , wy,			//Window starting coordinates
 		StatusBarHeight,	//Status Bar Height
 		ToolBarHeight,		//Tool Bar Height (distance from top of window to bottom line of toolbar)
